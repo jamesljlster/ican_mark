772a74d50af628fa7d78774b7bc133cde01feb0e
Zheng-Ling Lai <jamesljlster@gmail.com>
2020-02-02 01:14:44 +0800

    Finish aim crosshair drawing function and degree estimation

--- test/gui_test/markarea.cpp
+++ test/gui_test/markarea.cpp
@@ -1,5 +1,6 @@
 #include "markarea.h"
 
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 
@@ -11,8 +12,33 @@
 #include <QRectF>
 #include <Qt>
 
+#define __M_PI 3.14159265359
+
 using namespace std;
 using namespace ical_mark;
+
+void draw_aim_crosshair(QPaintDevice* widget, const QPoint& center,
+                        float degree, const QColor& penColor)
+{
+    int width = widget->width();
+    int height = widget->height();
+
+    // Setup painter and drawing style
+    QPainter painter(widget);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setCompositionMode(QPainter::RasterOp_SourceXorDestination);
+    painter.setPen(penColor);
+
+    // Draw aim crosshair
+    float shift = sin(degree * __M_PI / 180.0);
+    painter.drawLine(  //
+        QPoint(center.x() - shift * center.y(), 0),
+        QPoint(center.x() + shift * (height - center.y()), height));
+    painter.drawLine(  //
+        QPoint(0, center.y() + shift * center.x()),
+        QPoint(width, center.y() - shift * (width - center.x())));
+}
 
 MarkArea::MarkArea(QWidget* parent) : QWidget(parent)
 {
@@ -20,7 +46,7 @@ MarkArea::MarkArea(QWidget* parent) : QWidget(parent)
     this->setFocus();
 
     this->bgImage = QImage("color_map.png");
-    this->setCursor(Qt::BlankCursor);
+    // this->setCursor(Qt::BlankCursor);
 }
 
 bool MarkArea::event(QEvent* event)
@@ -28,11 +54,12 @@ bool MarkArea::event(QEvent* event)
     bool ret = false;
     QEvent::Type eventType = event->type();
 
+    // Run FSM of marking action
     if (eventType == QEvent::MouseMove)
     {
         QMouseEvent* me = static_cast<QMouseEvent*>(event);
         this->mousePos = me->pos();
-        this->testAction.run(me);
+        this->markAction.run(me);
         ret = true;
     }
     else if (eventType == QEvent::MouseButtonPress ||
@@ -41,7 +68,7 @@ bool MarkArea::event(QEvent* event)
         QMouseEvent* me = static_cast<QMouseEvent*>(event);
         if (me->button() == Qt::MouseButton::LeftButton)
         {
-            this->testAction.run(me);
+            this->markAction.run(me);
             ret = true;
         }
     }
@@ -50,42 +77,56 @@ bool MarkArea::event(QEvent* event)
         QKeyEvent* ke = static_cast<QKeyEvent*>(event);
         if (ke->key() == Qt::Key_Escape)
         {
-            this->testAction.revert();
+            this->markAction.revert();
             ret = true;
         }
         else if (ke->key() == Qt::Key_Backspace)
         {
-            this->testAction.reset();
+            this->markAction.reset();
             ret = true;
         }
     }
 
-    cout << "state: " << this->testAction.state();
-    switch (static_cast<RBoxMark::State>(this->testAction.state()))
+    // Status checking and processing
+    cout << "state: " << this->markAction.state();
+    switch (static_cast<RBoxMark::State>(this->markAction.state()))
     {
         case RBoxMark::State::INIT:
-            cout << "-" << this->testAction["degree"].state();
+            this->degree = 0;
+            cout << "-" << this->markAction["degree"].state();
             break;
 
         case RBoxMark::State::DEGREE_FIN:
-            cout << "-" << this->testAction["bbox"].state();
+            do
+            {
+                // Calculate degree
+                QPoint from = this->markAction["degree"]["pos1"]["release"];
+                QPoint to = this->markAction["degree"]["pos2"]["release"];
+                double radTmp = atan2(from.y() - to.y(), to.x() - from.x());
+                this->degree = radTmp * 180.0 / __M_PI - 90.0;
+            } while (0);
+
+            cout << "-" << this->markAction["bbox"].state();
+
             break;
 
         case RBoxMark::State::BBOX_FIN:
             break;
     }
 
-    if (this->testAction.finish())
+    cout << " degree: " << this->degree;
+
+    if (this->markAction.finish())
     {
         /*
-        printf(" [(%d, %d) (%d, %d)] ", this->testAction["pos1"]["press"].x(),
-               this->testAction["pos1"]["press"].y(),
-               this->testAction["pos1"]["release"].x(),
-               this->testAction["pos1"]["release"].y());
-        printf(" [(%d, %d) (%d, %d)] ", this->testAction["pos2"]["press"].x(),
-               this->testAction["pos2"]["press"].y(),
-               this->testAction["pos2"]["release"].x(),
-               this->testAction["pos2"]["release"].y());
+        printf(" [(%d, %d) (%d, %d)] ", this->markAction["pos1"]["press"].x(),
+               this->markAction["pos1"]["press"].y(),
+               this->markAction["pos1"]["release"].x(),
+               this->markAction["pos1"]["release"].y());
+        printf(" [(%d, %d) (%d, %d)] ", this->markAction["pos2"]["press"].x(),
+               this->markAction["pos2"]["press"].y(),
+               this->markAction["pos2"]["release"].x(),
+               this->markAction["pos2"]["release"].y());
        */
         cout << " Finished";
     }
@@ -107,8 +148,7 @@ void MarkArea::paintEvent(QPaintEvent* paintEvent)
     int width = this->width();
     int height = this->height();
 
-    QPoint center = this->mousePos;
-
+    // Paint background
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
@@ -119,14 +159,37 @@ void MarkArea::paintEvent(QPaintEvent* paintEvent)
     }
     else
     {
-        QRectF target(0, 0, width, height);
-        QRectF source(0, 0, this->bgImage.width(), this->bgImage.height());
-        painter.drawImage(target, this->bgImage, source);
+        QRectF bgRect(0, 0, width, height);
+        QRectF srcRect(0, 0, this->bgImage.width(), this->bgImage.height());
+        painter.drawImage(bgRect, this->bgImage, srcRect);
     }
 
-    painter.setCompositionMode(QPainter::RasterOp_SourceXorDestination);
-    painter.setPen(QColor(160, 160, 160));
+    // Draw aim crosshair
+    draw_aim_crosshair(this, this->mousePos, this->degree);
 
-    painter.drawLine(QPoint(center.x(), 0), QPoint(center.x(), height));
-    painter.drawLine(QPoint(0, center.y()), QPoint(width, center.y()));
+    // Draw marking
+    switch (static_cast<RBoxMark::State>(this->markAction.state()))
+    {
+        case RBoxMark::State::INIT:
+            switch (static_cast<TwiceClick::State>(
+                this->markAction["degree"].state()))
+            {
+                case TwiceClick::State::INIT:
+                    break;
+
+                case TwiceClick::State::POS1_FIN:
+                    break;
+
+                case TwiceClick::State::POS2_FIN:
+                    break;
+            }
+
+            break;
+
+        case RBoxMark::State::DEGREE_FIN:
+            break;
+
+        case RBoxMark::State::BBOX_FIN:
+            break;
+    }
 }
--- test/gui_test/markarea.h
+++ test/gui_test/markarea.h
@@ -7,9 +7,14 @@
 #include <QEvent>
 #include <QImage>
 #include <QObject>
+#include <QPaintDevice>
 #include <QWidget>
 
 #include <mark_action.hpp>
+
+void draw_aim_crosshair(QPaintDevice* widget, const QPoint& center,
+                        float degree,
+                        const QColor& penColor = QColor(160, 160, 160));
 
 class MarkArea : public QWidget
 {
@@ -23,10 +28,12 @@ class MarkArea : public QWidget
     bool event(QEvent* event);
     void paintEvent(QPaintEvent* paintEvent);
 
-    ical_mark::RBoxMark testAction;
+    ical_mark::RBoxMark markAction;
 
     QImage bgImage;
     QPoint mousePos;
+
+    float degree = 0;
 };
 
 #endif  // MARKAREA_H
